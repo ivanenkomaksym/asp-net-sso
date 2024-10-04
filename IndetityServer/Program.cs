a4071f7a40a42c13fd9d84a3a41f1b20823c318b
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-10-04 16:30:29 +0300

    Implement IdentityServer.

--- IndetityServer/Program.cs
+++ IndetityServer/Program.cs
@@ -1,16 +1,44 @@
+using Duende.IdentityServer.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(
+    [
+        new Client
+        {
+            ClientId = "client",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedScopes = { "api1" }
+        }
+    ])
+    .AddInMemoryApiScopes(new[]
+    {
+        new ApiScope("api1", "My API")
+    })
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
