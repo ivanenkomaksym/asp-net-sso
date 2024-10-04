cd0d3039e9038213d0138e3579a6fdcc912eccaf
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-10-04 16:43:38 +0300

    Add first IdentityServer integration tests.

---
+++ IdentityServer.IntegrationTests/ApplicationBase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Xunit.Abstractions;
+
+namespace IdentityServer.IntegrationTests
+{
+    internal class ApplicationBase : WebApplicationFactory<Program>
+    {
+        private readonly ITestOutputHelper Output;
+        public ApplicationBase(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        protected virtual IEnumerable<KeyValuePair<string, string>> GetMemoryConfiguration()
+        {
+            return [];
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(configurationBuilder =>
+            {
+                var configuration = GetMemoryConfiguration();
+                if (configuration == null)
+                    return;
+
+                configurationBuilder.Sources.Clear();
+                var fromMemory = new MemoryConfigurationSource { InitialData = configuration };
+                configurationBuilder.Add(fromMemory);
+            }).ConfigureServices(services =>
+            {
+                services.AddControllers().AddApplicationPart(typeof(Program).Assembly);
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
---
+++ IdentityServer.IntegrationTests/IdentityServerIntegrationTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IdentityServer.IntegrationTests
+{
+    public class IdentityServerIntegrationTests(ITestOutputHelper Output)
+    {
+        [Fact]
+        public async Task Can_Get_Token_From_IdentityServer()
+        {
+            // Arramge
+            using var application = new ApplicationBase(Output);
+            var client = application.CreateClient();
+
+            // Simulate the token request as curl would do
+            var requestBody = new StringContent("client_id=client&client_secret=secret&grant_type=client_credentials&scope=api1",
+                                                Encoding.UTF8,
+                                                "application/x-www-form-urlencoded");
+
+            // Act
+            var response = await client.PostAsync("/connect/token", requestBody);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Ensure the response is a success.
+
+            var content = await response.Content.ReadAsStringAsync();
+            var message = new OpenIdConnectMessage(content);
+
+            Assert.Equal("Bearer", message.TokenType);
+            Assert.Equal("api1", message.Scope);
+            Assert.NotEmpty(message.AccessToken);
+        }
+    }
+}
