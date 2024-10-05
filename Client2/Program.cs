57bab1d3a96e84a9bc4414701b87e19ef719b98d
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-10-05 10:40:06 +0300

    Implement clients.

--- Client2/Program.cs
+++ Client2/Program.cs
@@ -4,12 +4,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+.AddCookie("Cookies")
+.AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = "https://localhost:5001"; // IdentityServer4 URL
+    options.ClientId = "client1"; // Client ID registered in IdentityServer4
+    options.ClientSecret = "secret1"; // Client secret registered in IdentityServer4
+    options.ResponseType = "code";
+    options.Scope.Add("api1"); // Scopes requested by the client application
+});
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
